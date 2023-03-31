661cdbeeeb86293873c3e13ba40f72365cddc220
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-03-31 16:03:22 +0800

    Update firebase.ts

--- tabbage backup/src/components/firebase/config/firebase.ts
+++ tabbage backup/src/components/firebase/config/firebase.ts
@@ -9,12 +9,7 @@ import { getAuth, AuthCredential } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAE7ZfPHsRH4X47Az3jEZxgj6ywousiy0A",
-  authDomain: "tabbage-1a3ae.firebaseapp.com",
-  projectId: "tabbage-1a3ae",
-  storageBucket: "tabbage-1a3ae.appspot.com",
-  messagingSenderId: "1001298803811",
-  appId: "1:1001298803811:web:9bc07f908583f5d7374c56"
+  /* SECRET */
 };
 
 const app = initializeApp(firebaseConfig);
